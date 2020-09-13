a698fea643d6ac56da8101519c5f1734bc3ccbcc
Adam Plánský <adamplansky@gmail.com>
2020-09-13 21:34:30 +0200

    add waitgroups and mutexes

---
+++ concurency/waitgroup/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	//var wg sync.WaitGroup
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//	fmt.Println("1st goroutine sleeping...")
+	//	time.Sleep(1 * time.Second)
+	//}()
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//	fmt.Println("2nd goroutine sleeping...")
+	//	time.Sleep(2 * time.Second)
+	//}()
+	//wg.Wait()
+	//fmt.Println("All goroutines complete.")
+
+
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %v!\n", id)
+	}
+	const numGreeters = 5
+	var wg sync.WaitGroup
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
+}
